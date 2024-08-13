eb3fc6377ff6b4c328167518acd952d1f5f9259d
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-08-14 00:02:41 +0100

    app theme data

---
+++ lib/core/themes/theme_data.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:prudence/core/themes/text_theme.dart';
+import 'package:prudence/core/utils/colors.dart';
+
+class AppThemeData {
+  AppThemeData._();
+
+  // light theme
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: GoogleFonts.spaceGrotesk().fontFamily,
+    brightness: Brightness.light,
+    primaryColor: primary,
+    scaffoldBackgroundColor: white,
+    textTheme: AppTextTheme.lightTextTheme,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: lightGrey,
+      surfaceTintColor: lightGrey,
+    ),
+  );
+
+  // dark theme
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: GoogleFonts.spaceGrotesk().fontFamily,
+    brightness: Brightness.dark,
+    primaryColor: primary,
+    scaffoldBackgroundColor: black,
+    textTheme: AppTextTheme.darkTextTheme,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: black,
+      surfaceTintColor: black,
+    ),
+  );
+}
