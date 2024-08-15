1044e8cfb0f7f2659d9127462e26523b36df53f0
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-08-15 11:05:49 +0100

    modified shadow style

--- lib/core/style/shadow_style.dart
+++ lib/core/style/shadow_style.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:prudence/core/utils/colors.dart';
+import 'package:prudence/core/utils/helper_functions.dart';
 
 class ShadowStyle {
-  static final customShadow = BoxShadow(
-    color: lightGrey.withOpacity(0.1),
-    blurRadius: 50,
-    spreadRadius: 7,
-    offset: const Offset(0, 2),
-  );
+  final BuildContext context;
+
+  ShadowStyle(this.context);
+
+  BoxShadow get customShadow {
+    final dark = HelperFunctions.isDarkMode(context);
+
+    return BoxShadow(
+      color: dark ? softGrey.withOpacity(0.1) : darkGrey.withOpacity(0.1),
+      blurRadius: 10,
+      spreadRadius: 5,
+      offset: const Offset(0, 4),
+    );
+  }
 }
