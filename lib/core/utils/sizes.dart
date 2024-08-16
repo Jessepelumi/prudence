3f5973ea9d3ed36923fae677de1a204b055bfa82
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-08-16 01:59:12 +0100

    added new size

--- lib/core/utils/sizes.dart
+++ lib/core/utils/sizes.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 const double extraSmall = 4.0;
 const double small = 8.0;
 const double medium = 16.0;
+const double smallLarge = 18.0;
 const double large = 24.0;
 const double extraLarge = 32.0;
 
