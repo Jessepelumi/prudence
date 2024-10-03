d325c16a843804af77137710d83bd5860b6b1407
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-10-03 15:53:18 +0100

    empty views

---
+++ lib/views/manage/manage_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:prudence/views/shared/page_appbar.dart';
+
+class ManageView extends StatelessWidget {
+  const ManageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: PageAppbar(title: "Manage"),
+    );
+  }
+}
---
+++ lib/views/task/task_list_view.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
+import 'package:prudence/core/utils/sizes.dart';
+import 'package:prudence/views/shared/page_appbar.dart';
+
+class TaskListView extends StatelessWidget {
+  const TaskListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PageAppbar(
+        title: "Tasks",
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(medium),
+            child: GestureDetector(
+              onTap: () {},
+              child: const PhosphorIcon(
+                PhosphorIconsBold.list,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
