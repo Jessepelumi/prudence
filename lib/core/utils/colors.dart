5be9c2432960df4fb9543e5f15ccd5aeca9fddaf
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-10-03 15:51:40 +0100

    added new constant colors

---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+// neutral colors
+const white = Color(0xFFFFFFFF);
+const black = Color(0xFF171817);
+
+// grey
+const darkGrey = Color(0xFF45474B);
+const lightGrey = Color(0xFFF5F5F5);
+const softGrey = Color(0xFFB4B4B8);
+
+// favorite
+const gold = Color(0xFFFFC100);
