06bbfcabb2fbed60416910cc4f9d8e380140c1ec
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-10-03 18:15:10 +0100

    added themes

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:prudence/core/themes/theme_data.dart';
+import 'package:prudence/router/navigation.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      theme: AppThemeData.lightTheme,
+      darkTheme: AppThemeData.darkTheme,
+      home: const Navigation(),
+    );
   }
 }
