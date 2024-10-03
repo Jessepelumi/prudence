8d5944a0fe8070f864f2d51accc9c5ae07ff848b
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-10-03 15:49:24 +0100

    custom app bar

---
+++ lib/views/shared/custom_appbar.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
+import 'package:prudence/core/utils/sizes.dart';
+
+class CustomAppbar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppbar({
+    super.key,
+    this.actions,
+  });
+
+  final List<Widget>? actions;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      leading: Padding(
+        padding: const EdgeInsets.all(small),
+        child: GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: const PhosphorIcon(
+            PhosphorIconsBold.caretLeft,
+          ),
+        ),
+      ),
+      actions: actions,
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(
+        appbarHeight(),
+      );
+}
